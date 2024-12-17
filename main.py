0aedbac7a76c863050ab50854ded900fc9433084
2006-Alina <galina.aslanova@list.ru>
2024-12-17 23:05:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#"1st program.
+print("задача 1" )
+print(9**0.5*5)
+#"2nd program".
+print("задача 2")
+print(9.99>9.98,1000!=1000.1)
+#"3rd program".
+print(" задача 3")
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+#"4th program".
+print("задача 4")
+print(int(float("123.456")*10)%10)
